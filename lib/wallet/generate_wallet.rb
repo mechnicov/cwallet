d652940555aeb6da80f968bbb9aa7c5033270a2b
Edem Topuzov <keltikan@gmail.com>
2021-11-05 13:51:39 +0300

    Generate wallet

---
+++ lib/wallet/generate_wallet.rb
@@ -0,0 +1,26 @@
+require 'bitcoin'
+
+module Wallet
+  class GenerateWallet
+    class << self
+      def call
+        new.call
+      end
+    end
+
+    def call
+      generate_wallet
+    end
+
+    private
+
+    def generate_wallet
+      key = Bitcoin::Key.generate
+
+      {
+        key: key.priv,
+        address: key.addr,
+      }
+    end
+  end
+end
